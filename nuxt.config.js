2630bbbec072a7998a76f9a818301cc447a9fab7
Austin Corwin <austincorwin5@gmail.com>
2021-05-31 16:00:08 -0600

    another edit for nuxt.config

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,7 +6,7 @@ export default {
   target: 'static',
 
   router: {
-    base: '/liquidmiles-dev/'
+    base: '/liquidmiles-dev/',
   }
 
   // Global page headers: https://go.nuxtjs.dev/config-head
