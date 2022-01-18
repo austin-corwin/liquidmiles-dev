a61db52e5f651b869d0bec180d82f7584ea318b3
MoxieAustin <austin.corwin@moxiesozo.com>
2022-01-18 14:39:28 -0700

    Begin setup for firebase

---
+++ functions/index.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+const { initializeApp } = require("firebase/app") ;
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBzSt2PJsYvKaSlJwALdpE0jsUYMpM56SA",
+  authDomain: "liquid-miles.firebaseapp.com",
+  projectId: "liquid-miles",
+  storageBucket: "liquid-miles.appspot.com",
+  messagingSenderId: "255238661987",
+  appId: "1:255238661987:web:3508466ad650b0e0395556"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
