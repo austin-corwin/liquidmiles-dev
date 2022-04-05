21bdffc8d655580a466ecd12658561dfdae697d5
Austin Corwin <austincorwin5@gmail.com>
2022-04-05 07:57:04 -0600

    Create test page for form

---
+++ pages/test.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <Test />
+  </div>
+</template>
+
+<script>
+import Test from '../components/Test'
+
+export default {
+  components: {
+    Test,
+  },
+  head() {
+    return {
+      title: 'Liquid Miles',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'The race of the century',
+        },
+      ],
+    }
+  },
+}
+</script>
