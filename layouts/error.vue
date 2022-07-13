81b01ba4f1259cc8884794a4ecb1a141b511c281
Austin Corwin <austincorwin5@gmail.com>
2022-07-12 21:15:28 -0600

    Change error code

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="d-flex justify-content-center align-items-center error">
     <h1 v-if="error.statusCode === 404">
-      Hm, looks like you're lost. Maybe Kona can help you find where you need to
-      go.
+      Hm, looks like you're lost. Might've drank too much. No worries, we'll
+      help you get home.
     </h1>
     <h1 v-else>An error occurred</h1>
     <NuxtLink to="/">Home page</NuxtLink>
